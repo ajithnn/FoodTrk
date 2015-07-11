13953c19d6f34be0d9a3725db0da7208864155e5
ajithnn <ajithnn20@gmail.com>
2015-07-11 21:59:56 +0530

    Markers and InfoWindows

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,51 +1,77 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener("deviceready", function() {
+        var mapDiv = document.getElementById("map_canvas");
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
-};
+        var map = plugin.google.maps.Map.getMap(mapDiv);
 
-app.initialize();
+
+
+        map.addEventListener(plugin.google.maps.event.MAP_READY, function() {
+                var markers = [];
+                var myLoc;
+                map.getMyLocation(function(location) {
+                        myLoc = location;
+                        alert(location.latLng.lat + 1);
+                        const Lat_1 = new plugin.google.maps.LatLng(location.latLng.lat + 1, location.latLng.lng + 1);
+                        const Lat_2 = new plugin.google.maps.LatLng(location.latLng.lat + 2, location.latLng.lng + 1);
+                        var msg = ["Current your location:\n",
+                            "latitude:" + location.latLng.lat,
+                            "longitude:" + location.latLng.lng
+                            ];
+
+                            map.addMarker({
+                                'position': location.latLng,
+                                'title': msg
+                            }, function(marker) {
+                                markers.push(marker);
+                                marker.showInfoWindow();
+                            });
+
+                            map.animateCamera({
+                                'target': location.latLng,
+                                'tilt': 60,
+                                'zoom': 18,
+                                'bearing': 140
+                            });
+
+                            map.addMarker({
+                                'position': Lat_1,
+                                'title': "Truck 1",
+                                'snippet': 'Kobri',
+                                'styles': {
+                                    'text-align': 'center',
+                                    'font-style': 'italic',
+                                    'font-weight': 'bold',
+                                    'color': 'red'
+                                }
+                            }, function(marker) {
+                                markers.push(marker);
+                                marker.addEventListener(plugin.google.maps.event.INFO_CLICK, function() {
+                                    alert("Today's Special is IdlyWich");
+                                });
+                            });
+
+                            map.addMarker({
+                                'position': Lat_2,
+                                'title': "Truck 2",
+                                'snippet': 'FlavorLabs',
+                                'styles': {
+                                    'text-align': 'center',
+                                    'font-style': 'italic',
+                                    'font-weight': 'bold',
+                                    'color': 'red'
+                                }
+                            }, function(marker) {
+                                markers.push(marker);
+                                markers[0].showInfoWindow();
+                                //markers[1].showInfoWindow();
+                                marker.addEventListener(plugin.google.maps.event.INFO_CLICK, function() {
+                                    alert("Today's Special is Burrito Masala.");
+                                });
+                            });
+
+
+                        });
+
+                });
+        });
